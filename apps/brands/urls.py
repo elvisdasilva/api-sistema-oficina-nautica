540bafd6cf347984f8e8c8496f00b09a412aaa88
Elvis da Silva <elvisdasilvademenezes@hotmail.com>
2024-05-28 17:55:55 -0300

    refactor: atualizar nome da rota brand-list-create

--- apps/brands/urls.py
+++ apps/brands/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views as views
 
 urlpatterns = [
-    path("brands/", views.BrandListCreateView.as_view(), name="teste"),
+    path("brands/", views.BrandListCreateView.as_view(), name="brand-list-create"),
 ]
