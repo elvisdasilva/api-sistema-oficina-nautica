2c3c2ffd699fea87d8711962e58bc41a5eebefc0
Elvis da Silva <elvisdasilvademenezes@hotmail.com>
2024-05-28 17:48:58 -0300

    feat: adicionar método __str__  na model Brand

--- apps/brands/models.py
+++ apps/brands/models.py
@@ -5,3 +5,6 @@ class Brand(models.Model):
     description = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.description
