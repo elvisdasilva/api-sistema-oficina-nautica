e8431c5b3087cf486ea270d880ce3608df5eec03
Elvis da Silva <elvisdasilvademenezes@hotmail.com>
2024-05-28 22:38:25 -0300

    feat: criar BrandRetrieveUpdateDestroyView

--- apps/brands/urls.py
+++ apps/brands/urls.py
@@ -3,4 +3,5 @@ from . import views as views
 
 urlpatterns = [
     path("brands/", views.BrandListCreateView.as_view(), name="brand-list-create"),
+    path("brands/<int:pk>/", views.BrandRetrieveUpdateDestroyView.as_view(), name="brand-detail-view")
 ]
--- apps/brands/views.py
+++ apps/brands/views.py
@@ -7,3 +7,8 @@ from apps.brands.serializers import BrandSerializer
 class BrandListCreateView(generics.ListCreateAPIView):
     queryset = Brand.objects.all()
     serializer_class = BrandSerializer
+
+
+class BrandRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Brand.objects.all()
+    serializer_class = BrandSerializer
