0d7764009a3afd18e6a7c9bab01a72dd9e48400e
Elvis da Silva <elvisdasilvademenezes@hotmail.com>
2024-06-02 11:42:57 -0300

    feat: adicionar validação de quantidade de caractéres

--- apps/brands/serializers.py
+++ apps/brands/serializers.py
@@ -6,3 +6,9 @@ class BrandSerializer(serializers.ModelSerializer):
     class Meta:
         model = Brand
         fields = "__all__"
+
+    def validate_description(self, value):
+        if len(value) > 50:
+            raise serializers.ValidationError(
+                "Description must not be longer than 50 characters"
+            )
