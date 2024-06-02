a54170abafed73f24fae8a8e26690deb1d4eae58
Elvis da Silva <elvisdasilvademenezes@hotmail.com>
2024-06-02 11:46:24 -0300

    feat: criar update, delete e detail de model

--- apps/models/views.py
+++ apps/models/views.py
@@ -6,3 +6,8 @@ from apps.models.serializers import ModelSerializer
 class ModelListCreateView(generics.ListCreateAPIView):
     queryset = Model.objects.all()
     serializer_class = ModelSerializer
+
+
+class ModelRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Model.objects.all()
+    serializer_class = ModelSerializer
