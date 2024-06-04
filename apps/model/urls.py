1c8dd5cc62f81d59be994d60b98487e6c59631f5
Elvis da Silva <elvisdasilvademenezes@hotmail.com>
2024-06-03 22:36:34 -0300

    feat: criar rota model-detail-update-delete

--- apps/model/urls.py
+++ apps/model/urls.py
@@ -3,5 +3,6 @@ from apps.model import views as views
 
 
 urlpatterns = [
-    path("models/", views.ModelListCreateView.as_view(), name="model-detail-view"),
+    path("models/", views.ModelListCreateView.as_view(), name="model-list-create"),
+    path("models/<int:pk>/", views.ModelRetrieveUpdateDestroyView.as_view(), name="model-detail-update-delete"),
 ]
