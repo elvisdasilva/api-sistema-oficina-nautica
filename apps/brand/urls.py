ffd3f5aaccbe708c4f5273896d06b1e3771839f4
Elvis da Silva <elvisdasilvademenezes@hotmail.com>
2024-06-03 22:36:59 -0300

    refactor: mudar o nome das rotas para algo mais legível

--- apps/brand/urls.py
+++ apps/brand/urls.py
@@ -3,5 +3,5 @@ from . import views as views
 
 urlpatterns = [
     path("brands/", views.BrandListCreateView.as_view(), name="brand-list-create"),
-    path("brands/<int:pk>/", views.BrandRetrieveUpdateDestroyView.as_view(), name="brand-detail-view")
+    path("brands/<int:pk>/", views.BrandRetrieveUpdateDestroyView.as_view(), name="brand-detail-update-delete"),
 ]
