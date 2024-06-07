44688e2c0af01c531aaf757ccd1eafd78a451a4a
Elvis da Silva <elvisdasilvademenezes@hotmail.com>
2024-06-06 21:47:56 -0300

    feat: criar rotas crud da app employee

--- apps/employee/urls.py
+++ apps/employee/urls.py
@@ -1,3 +1,7 @@
 from django.urls import path
+from apps.employee import views as views
 
-urlpatterns = []
+urlpatterns = [
+    path("employees/", views.EmployeeListCreateView.as_view(), name="employee-list-create"),
+    path("employees/<int:pk>/", views.EmployeeRetrieveUpdateDestroyView.as_view(), name="employee-detail-update-delete"),
+]
