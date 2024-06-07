c28f74efccec7eee8d0475cbeb13a10efc158ff0
Elvis da Silva <elvisdasilvademenezes@hotmail.com>
2024-06-06 21:05:39 -0300

    refactor: criar migrations de customer com cpf, rg e cnpj com unique

--- apps/customer/models.py
+++ apps/customer/models.py
@@ -54,29 +54,30 @@ class Customer(models.Model):
     ]
 
     full_name = models.CharField(max_length=100)
-    fancy_name = models.CharField(max_length=100, blank=True, null=True)
+    fancy_name = models.CharField(max_length=100, blank=True)
     address = models.CharField(max_length=100)
     district = models.CharField(max_length=50)
-    address_complement = models.CharField(max_length=100, blank=True, null=True)
+    address_complement = models.CharField(max_length=100, blank=True)
     city = models.CharField(max_length=50)
     state = models.CharField(choices=STATE_CHOICES, max_length=2)
-    zip_code = models.CharField(max_length=10, blank=True, null=True)
-    first_telephone = models.CharField(max_length=20, blank=True, null=True)
-    second_telephone = models.CharField(max_length=20, blank=True, null=True)
+    zip_code = models.CharField(max_length=10, blank=True)
+    first_telephone = models.CharField(max_length=20, blank=True)
+    second_telephone = models.CharField(max_length=20, blank=True)
     phone_number = models.CharField(max_length=20)
-    whatsapp = models.CharField(max_length=20, blank=True, null=True)
-    email = models.CharField(max_length=100, blank=True, null=True)
+    whatsapp = models.CharField(max_length=20, blank=True)
+    email = models.EmailField(max_length=100, blank=True)
     contato = models.CharField(max_length=100)
-    gender = models.CharField(
-        choices=GENDER_CHOICES, max_length=1, blank=True, null=True
-    )
-    date_of_birth = models.DateField(blank=True, null=True)
-    cpf = models.CharField(max_length=14, unique=True, blank=True, null=True)
-    cnpj = models.CharField(max_length=18, unique=True, blank=True, null=True)
-    rg = models.CharField(max_length=20, unique=True, blank=True, null=True)
-    issuing_authority = models.CharField(max_length=100, blank=True, null=True)
+    gender = models.CharField(choices=GENDER_CHOICES, max_length=1, blank=True)
+    date_of_birth = models.DateField(blank=True)
+    cpf = models.CharField(max_length=14, unique=True, null=True)
+    cnpj = models.CharField(max_length=18, unique=True, null=True)
+    rg = models.CharField(max_length=20, unique=True, null=True)
+    issuing_authority = models.CharField(max_length=100, blank=True)
     status = models.CharField(choices=STATUS_CHOICES, max_length=2, default="AT")
-    general_observations = models.TextField(max_length=500, blank=True, null=True)
+    general_observations = models.TextField(blank=True)
     registration_type = models.CharField(
         choices=REGISTRATION_TYPE_CHOICES, max_length=2
     )
+
+    def __str__(self):
+        return self.full_name
