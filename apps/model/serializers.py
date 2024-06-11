b59db5c26dc856c7b2fc6197204a6d0b8242ff15
Elvis da Silva <elvisdasilvademenezes@hotmail.com>
2024-06-10 22:43:10 -0300

    refactor: remover validação no serializer

--- apps/model/serializers.py
+++ apps/model/serializers.py
@@ -6,10 +6,3 @@ class ModelSerializer(serializers.ModelSerializer):
     class Meta:
         model = Model
         fields = "__all__"
-
-    def validate_description(self, value):
-        if len(value) > 50:
-            raise serializers.ValidationError(
-                "Description must not be longer than 50 characters"
-            )
-        return value
