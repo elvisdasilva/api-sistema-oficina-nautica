3dfd2df50c8d0ac3de9b9ed3ea1eb41a77d23243
Elvis da Silva <elvisdasilvademenezes@hotmail.com>
2024-06-10 22:01:39 -0300

    feat: configurar STATIC_ROOT e MEDIA_ROOT

--- core/urls.py
+++ core/urls.py
@@ -14,14 +14,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/v1/', include('apps.brand.urls')),
-    path('api/v1/', include('apps.model.urls')),
-    path('api/v1/', include('apps.customer.urls')),
-    path('api/v1/', include('apps.employee.urls')),
-    path('api/v1/', include('apps.vessel.urls')),
+    path("admin/", admin.site.urls),
+    path("api/v1/", include("apps.brand.urls")),
+    path("api/v1/", include("apps.model.urls")),
+    path("api/v1/", include("apps.customer.urls")),
+    path("api/v1/", include("apps.employee.urls")),
+    path("api/v1/", include("apps.vessel.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
